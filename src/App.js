8f70582af7b23fca274f8c3ab4646bd6b482a527
TungMoc <tung3qss123@gmail.com>
2022-08-25 00:06:34 +0700

    up-to-date everything 24/08/2022

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayOut } from '~/components/Layout';
+import { Fragment } from 'react';
 function App() {
    return (
-      <div className="App">
-         <h1>React App</h1>
-      </div>
+      <Router>
+         <div className="App">
+            <Routes>
+               {publicRoutes.map((route, index) => {
+                  const Page = route.component;
+                  let Layout = DefaultLayOut;
+                  if (route.layout) {
+                     Layout = route.layout;
+                  } else if (route.layout === null) {
+                     Layout = Fragment;
+                  }
+
+                  return (
+                     <Route
+                        key={index}
+                        path={route.path}
+                        element={
+                           <Layout>
+                              <Page />
+                           </Layout>
+                        }
+                     />
+                  );
+               })}
+            </Routes>
+         </div>
+      </Router>
    );
 }
 
